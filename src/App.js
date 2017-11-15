08e998ea91f31cb81130409abe60fd4b5c99b87f
Paul Wallas <paulwallas@me.com>
2017-11-15 21:15:38 +0000

    index markup changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="title">Hi, I'm Paul.</h1>
+        <p className="content">Head of Design at <a href="http://www.rightindem.com">RightIndem</a></p>
+        <p className="content">I create simple and intuitive interfaces that are clean and free of complexity.</p>
+        <hr />
+        <p className="content">You can follow me on <a href="https://twitter.com/paulwallas">twitter</a> and <a href="https://medium.com/@paulwallas">medium</a>.</p>
       </div>
     );
   }
