5df8fed28bfb6143a0acf399ff809f4d0cebac5b
Paul Wallas <paulwallas@me.com>
2017-11-15 21:21:41 +0000

    removed dot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="title">Hi, I'm Paul.</h1>
+        <h1 className="title">Hi, I'm Paul</h1>
         <p className="content">Head of Design at <a href="http://www.rightindem.com">RightIndem</a></p>
         <p className="content">I create simple and intuitive interfaces that are clean and free of complexity.</p>
         <hr />
