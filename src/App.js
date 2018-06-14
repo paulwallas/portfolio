4de1e48cdc202cc5f0725313d7bf70e57228ca7d
Paul Wallas <paulwallas@me.com>
2018-06-14 22:45:49 +0100

    added more work to portfolio

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import me from './assets/me-at-whiteboard.png';
 import admiralApp from './assets/admiral-app.png';
 import carePartner from './assets/care-partner-website.png';
 import bumpApp from './assets/bump-app.png';
-import healthApp from './assets/health-app.png';
+import drank from './assets/drank.png';
+import watchLater from './assets/watch-it-later.png';
+import parkr from './assets/parkr.png';
 import moneyApp from './assets/money-app.png';
+import deloitte from './assets/deloitte-app.png';
+import rightindem from './assets/rightindem-website.png';
 
 class App extends Component {
   render() {
@@ -19,12 +23,59 @@ class App extends Component {
 
         <section className="wrapperOuter fadeIn">
           <div className="work">
-            <img src={me} className="workItem" alt="me-at-whiteboard"/>
-            <img src={admiralApp} className="workItem" alt="admiral-claim-app"/>
-            <img src={carePartner} className="workItem" alt="care-partner-website"/>
-            <img src={bumpApp} className="workItem" alt="bump-and-go-app"/>
-            <img src={moneyApp} className="workItem" alt="money-app"/>
-            <img src={healthApp} className="workItem" alt="health-app"/>
+            <div className="workItem1">
+            <img src={me} alt="me-at-whiteboard"/>
+            </div>
+            <div className="workItem2">
+              <img src={moneyApp} alt="money-tracker-app"/>
+              <img src={bumpApp} alt="bump-and-go-app"/>
+            </div>
+          </div>
+        </section>
+
+        <section className="wrapperInner fadeIn">
+            <p className="info">I have produced work for global market leaders such as Deloitte, Admiral and XLCatlin, having the ability to meet and communicate design ideas to key client stakeholders.</p>
+        </section>
+
+        <section className="wrapperOuter fadeIn">
+          <div className="work doubleCol">
+            <div className="workItem1">
+            <img src={deloitte} alt="deloitte-app"/>
+            </div>
+            <div className="workItem2">
+              <img src={admiralApp} alt="admiral-app"/>
+            </div>
+          </div>
+        </section>
+
+        <section className="wrapperInner fadeIn">
+            <p className="info">I’m experienced creating white-label interfacing products and products that adhere to existing brand guidelines. I have created, maintained and evolved several brands throughout my career.</p>
+        </section>
+
+        <section className="wrapperOuter fadeIn">
+          <div className="work doubleCol">
+            <div className="workItem1">
+            <img src={carePartner} alt="care-partner-website"/>
+            </div>
+            <div className="workItem2">
+              <img src={rightindem} alt="rightindem-website"/>
+            </div>
+          </div>
+        </section>
+
+        <section className="wrapperInner fadeIn">
+            <p className="info">I’m skilled across the entire design domain having produced web applications, websites, iOS apps, branding guidelines, off-screen material and illustrations in my career to date.</p>
+        </section>
+
+        <section className="wrapperOuter fadeIn">
+          <div className="work">
+            <div className="workItem1">
+            <img src={watchLater} alt="watch-it-later-tvos-icon"/>
+            </div>
+            <div className="workItem2">
+              <img src={drank} alt="frank-ios-icon"/>
+              <img src={parkr} alt="parkr-ios-icon"/>
+            </div>
           </div>
         </section>
 
