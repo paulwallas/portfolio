106de40b787addfc366916102653ab60ea8fa230
paulwallas <paulwallas@me.com>
2018-10-19 21:07:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
             <h1 className="title">Paul Wallas</h1>
             <p className="info">User Interface &amp; User Experience Designer.<br /><br />
             Over 10 years experience designing and delivering user-centric solutions, advocating the importance of user experience. I’ve helped companies improve their customer communication by enhancing and/or redesigning their brand message and managed teams of designers helping to develop individual skillsets.<br /><br />
-            Although specialising in user interface and user experience design, I’m a multi-discipled designer with skills in illustration, print, iconography and iOS application design.</p>
+            Although specialising in user interface and user experience design, I’m a multi-discipled designer with skills in illustration, print, iconography and iOS application design.<br /><br />
+            Work samples can be <a href="mailto:paulwallas@me.com">sent on request</a>.</p>
         </div>
 
       </div>
