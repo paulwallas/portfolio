b6a37f05bca0f5b4f43a51f929592b8b3ca23306
paulwallas <paulwallas@me.com>
2019-10-29 20:23:18 +0000

    Added work preview

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <p className="info">User Interface &amp; User Experience Designer.<br /><br />
             Over 10 years experience designing and delivering user-centric solutions, advocating the importance of user experience. I’ve helped companies improve their customer communication by enhancing and/or redesigning their brand message and managed teams of designers helping to develop individual skillsets.<br /><br />
             Although specialising in user interface and user experience design, I’m a multi-discipled designer with skills in illustration, print, iconography and iOS application design.<br /><br />
-            Work samples can be <a href="mailto:paulwallas@me.com">sent on request</a>.</p>
+            Work samples can be <a href="mailto:paulwallas@me.com">sent on request</a> or you can sample a <a href="">preview here</a>.</p>
         </div>
 
       </div>
