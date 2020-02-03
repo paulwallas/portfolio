08636d8dc2f5f246d6ee944ef2d5542dd4064561
Paul Wallas <paulwallas@me.com>
2020-02-03 15:04:35 +0000

    removed paragraph

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ class App extends Component {
         
         <div className="flex-item fadeIn fadeUp">
             <h1 className="title">Paul Wallas</h1>
-            <p className="info">User Interface &amp; User Experience Designer.<br /><br />
-            Over 10 years experience designing and delivering user-centric solutions, advocating the importance of user experience. I’ve helped companies improve their customer communication by enhancing and/or redesigning their brand message and managed teams of designers helping to develop individual skillsets.<br /><br />
-            Although specialising in user interface and user experience design, I’m a multi-discipled designer with skills in illustration, print, iconography and iOS application design.<br /><br />
-            Work samples can be <a href="mailto:paulwallas@me.com">sent on request</a> or you can sample a <a href="">preview here</a>.</p>
+            <p className="info">User Interface &amp; User Experience Designer.</p>
         </div>
 
       </div>
